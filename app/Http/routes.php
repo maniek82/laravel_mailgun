cbe3fcb607b96a9ac5deecbb172e4a5bab650dc0
Mariusz <mariusz.tripler@gmail.com>
2016-09-02 16:23:17 +0000

    added usun.txt

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -4,12 +4,12 @@ use Illuminate\Support\Facades\Mail;
 
 Route::get('/', function () {
     $data = [
-        'title'=>'Czesc Aurelko',
-        'content'=>'Sprawdz jak mozesz odebra ta nagrode:)'
+        'title'=>'heck this email',
+        'content'=>' How to send email from laravel via mailgun'
         
         ];
-        // Mail::send('emails.test',$data, function($message) {
-        //     $message->to('mariusz.tripler@gmail.com','Mariusz')->subject('Wygralas buziaka');
-        // });
+        Mail::send('emails.test',$data, function($message) {
+            $message->to('mariusz.tripler@gmail.com','Mariusz')->subject('Testing email features');
+        });
     
 });
